f5197752630ed98a3e9e6ccf6aada11e85f1ba62
Ekaterina <fedorovaea18@yandex.ru>
2024-01-29 19:03:06 +0300

    add help

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-public class App {
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionRequested;
+
+    @Override
+    public void run() {
+        if (helpRequested) {
+            CommandLine.usage(this, System.out);
+        } else if (versionRequested) {
+            CommandLine versionCommand = new CommandLine(this.getClass());
+            versionCommand.printVersionHelp(System.out);
+        } else {
+            // Add your logic for comparing two configuration files here
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CommandLine.run(new App(), args);
     }
 }
