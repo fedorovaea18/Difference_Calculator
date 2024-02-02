c375251a021b0c26746d0c78e576a822fc9c01e2
Ekaterina <fedorovaea18@yandex.ru>
2024-02-02 12:25:59 +0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("java")
     application
+    checkstyle
 }
 
 group = "hexlet.code"
@@ -8,6 +8,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+tasks.compileJava {
+    options.release = 20
 }
 
 application {
@@ -18,8 +22,11 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation ("info.picocli:picocli:4.7.5")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.14.0-rc1")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,15 +1,29 @@
 package hexlet.code;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable {
+class App implements Callable<String> {
 
     @Option(names = {"-f", "--format"}, defaultValue = "stylish", description = "output format [default: stylish]")
     private String format;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionInfoRequested;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean usageHelpRequested;
 
     @Parameters(paramLabel = "filepath1", description = "path to first file")
     private String filepath1;
@@ -18,10 +32,21 @@ public class App implements Runnable {
     private String filepath2;
 
     @Override
-    public void run() {
+    public String call() throws Exception {
+        Path path1 = Paths.get(filepath1).toAbsolutePath().normalize();
+        Path path2 = Paths.get(filepath2).toAbsolutePath().normalize();
+        String contentFile1 = Files.readString(path1);
+        String contentFile2 = Files.readString(path2);
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map1 = mapper.readValue(contentFile1, Map.class);
+        Map<String, Object> map2 = mapper.readValue(contentFile2, Map.class);
+        String diff = Differ.generate(map1, map2);
+        System.out.println(diff);
+        return diff;
     }
 
     public static void main(String[] args) {
-        CommandLine.run(new App(), args);
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
