f43a00da3f91516e1bcb1b9c826a9ad36d8b1460
Ekaterina <fedorovaea18@yandex.ru>
2024-02-15 11:01:57 +0300

    added new class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -21,37 +21,8 @@ public class Differ {
         Map<String, Object> map1 = Parser.parse(data1, fileType1);
         Map<String, Object> map2 = Parser.parse(data2, fileType2);
 
-        List<Map<String, Object>> result = comparator(map1, map2);
+        List<Map<String, Object>> result = CompareMaps.comparator(map1, map2);
         return Formatter.formatName(result, formatName);
-    }
-
-    public static List<Map<String, Object>> comparator(Map<String, Object> map1, Map<String, Object> map2) {
-        List<Map<String, Object>> result = new ArrayList<>();
-        Set<String> keysSet = new TreeSet<>(map1.keySet());
-        keysSet.addAll(map2.keySet());
-        for (String key : keysSet) {
-            Map<String, Object> map = new LinkedHashMap<>();
-            if (map1.containsKey(key) && !map2.containsKey(key)) {
-                map.put("key", key);
-                map.put("oldValue", map1.get(key));
-                map.put("type", "removed");
-            } else if (!map1.containsKey(key) && map2.containsKey(key)) {
-                map.put("key", key);
-                map.put("newValue", map2.get(key));
-                map.put("type", "added");
-            } else if (!Objects.equals(map1.get(key), map2.get(key))) {
-                map.put("key", key);
-                map.put("oldValue", map1.get(key));
-                map.put("newValue", map2.get(key));
-                map.put("type", "updated");
-            } else {
-                map.put("key", key);
-                map.put("oldValue", map1.get(key));
-                map.put("type", "unchanged");
-            }
-            result.add(map);
-        }
-        return result;
     }
 
     public static String getData(String filepath) {
